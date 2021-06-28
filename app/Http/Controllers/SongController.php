ee902f09bbe4b619cac38836d454e79b19d2eb3e
monmima <monmima@protonmail.com>
2021-06-28 17:13:19 -0400

    support create route for songs

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -36,7 +36,7 @@ class SongController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Produit  $produit
+     * @param  \App\Models\Song  $song
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -44,7 +44,7 @@ class SongController extends Controller
         //
 
         $song = Song::findOrFail($id);
-        // $categories = ProduitCategorie::all();
+        // $categories = SongCategorie::all();
 
         // $validated = $request->validate([
         //     // this rule makes it possible to change the name of an item as long as the it remains unique
@@ -58,18 +58,55 @@ class SongController extends Controller
         //     // "categorie" => "required"
         // ]);
 
-        // updating produit table
+        // updating song table
         $song->name = request("name");
         $song->artist = request("artist");
         $song->lyrics = request("lyrics");
         $song->link = request("link");
 
-        // $produit->categorie = request("categorie");
+        // $song->categorie = request("categorie");
         $song->save();
 
         // back to the main page
         $song = Song::all();
 
         return redirect("/");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $validated = $request->validate([
+            "name" => "required|min:5|max:255|unique:songs",
+            "artist" => "required",
+            "lyrics" => "required",
+            "link" => "required",
+            // "categorie" => "required"
+        ]);
+
+        //
+
+        $song = new Song();
+
+        // 1. the basic way
+        $song->name = request("name");
+        $song->artist = request("artist");
+        $song->lyrics = request("lyrics");
+        $song->link = request("link");
+        $song->save();
+
+        // --processing categories-- //
+
+
+
+        // --end processing categories-- //
+
+        return redirect("/")->with("msg", "Song was added");
     }
 }
