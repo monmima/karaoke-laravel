5fb7f5f739c6afbc8304f752aa9dea764478963f
monmima <monmima@protonmail.com>
2021-06-30 18:13:34 -0400

    created many-to-many relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Song extends Model
+class Category extends Model
 {
     use HasFactory;
 
-    public function categories()
+    public function songs()
     {
-            return $this->belongsToMany(SongCategory::class);
+            return $this->belongsToMany(Song::class);
     }
 }
