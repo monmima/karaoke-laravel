99b59b6e3df040f7d64ad28b7573715d5c52e11f
monmima <monmima@protonmail.com>
2021-07-14 10:35:48 -0400

    add list of filters to index page; add links to the page on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,8 @@
         <h1>Nés pour chanter</h1>
 
         <p><a href="/categories" title="Catégories de chansons">Voir les catégories</a>
+        <p><a href="/test" title="Test 1">Test 1</a>
+        <p><a href="/test2" title="Test 2">Test 2</a>
     </header>
 
     <main>
@@ -54,6 +56,14 @@
         </section>
 
         <aside>
+
+            <h3>Filters</h3>
+
+            @foreach($categories as $categorie)
+                <input type="checkbox" id="{{ $categorie->id }}" name="{{ $categorie->id }}" value="{{ $categorie->title }}">
+                <label for="{{ $categorie->id }}">{{ $categorie->title }}</label><br>
+            @endforeach
+
             <h3>Create New Entry</h3>
 
             <form method="POST" name="edit-song" id="edit-song" action="/create">
