f14191c12e509af808cc2049e32e552e5613e15b
monmima <monmima@protonmail.com>
2021-07-21 17:54:26 -0400

    test to see if loading assets with both HTTP and HTTPS will run locally and remotely

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nés pour chanter</title>
 
+    {{-- <link rel="stylesheet" href="{{asset('css/style.css')}}" /> --}}
     <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+    <link rel="stylesheet" href="{{secure_asset('css/style.css')}}" />
 </head>
 <body>
     <header>
