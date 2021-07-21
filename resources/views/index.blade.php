64c385c252412e120da947e803cd5fae8ae67a5c
monmima <monmima@protonmail.com>
2021-07-21 17:57:35 -0400

    add comment on the fact assets are being loaded twice

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nés pour chanter</title>
 
-    {{-- <link rel="stylesheet" href="{{asset('css/style.css')}}" /> --}}
+    <!-- loading the assets twice like this is nasty as hell, but it works -->
     <link rel="stylesheet" href="{{asset('css/style.css')}}" />
     <link rel="stylesheet" href="{{secure_asset('css/style.css')}}" />
 </head>
